4f4e25dd352b9a12b26361d63af686823fe8e79c
bhumishah2061 <bhumishah2061@gmail.com>
2025-05-11 19:56:19 +0545

    second code added

---
+++ number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int random, guess, count = 0;
+
+    srand(time(0)); // initialize random seed
+    random = rand() % 100; // generate random number between 0 and 99
+
+    printf("Guess a number between 0 and 99\n");
+
+    while (1) {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        count++;
+
+        if (guess > random) {
+            printf("Too high! Try a lower number.\n");
+        } else if (guess < random) {
+            printf("Too low! Try a higher number.\n");
+        } else {
+            printf("Correct! You guessed it in %d tries.\n", count);
+            break;
+        }
+    }
+
+    return 0;
+}
